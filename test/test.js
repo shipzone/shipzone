4c14b8a3594932c55a21a037a55525d2f520bf01
PhilKunz <github@philkunz.com>
2016-02-18 16:55:33 +0100

    initial

---
+++ test/test.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJ0ZXN0LmpzIiwic291cmNlc0NvbnRlbnQiOltdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
